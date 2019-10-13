c5addacf28835433023da2ede1289f6956b80019
qq188380780 <188380780@qq.com>
2019-10-13 22:11:09 +0800

    MainActivity

--- app/src/main/java/xin/jeson/MainActivity.java
+++ app/src/main/java/xin/jeson/MainActivity.java
@@ -1,14 +1,29 @@
 package xin.jeson;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import xin.jeson.Adapter.LinearAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView mRvMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRvMain = findViewById(R.id.mRvMain);
+        mRvMain.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+        mRvMain.setAdapter(new LinearAdapter(MainActivity.this, new LinearAdapter.OnItemClickListener() {
+            @Override
+            public void onClick(int pos) {
+                Toast.makeText(MainActivity.this, "hello", Toast.LENGTH_SHORT).show();
+            }
+        }));
     }
+
 }
